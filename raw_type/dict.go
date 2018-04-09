736ee26afd901549ad1ef354bb384e0f04f8e0fa
李洺吉 <mingjili@creditease.cn>
2018-04-09 16:40:01 +0800

    #20 implements a dict

--- raw_type/dict.go
+++ raw_type/dict.go
@@ -1,6 +1,8 @@
 package raw_type
 
-import "github.com/mitchellh/hashstructure"
+import (
+	"github.com/mitchellh/hashstructure"
+)
 
 const (
 	/**
@@ -17,27 +19,55 @@ const (
 	LoadFactory = 0.75
 )
 
+/************************************  dictEntry  ***************************************/
 type dictEntry struct {
-	key   interface{}
-	value interface{}
-	next  *dictEntry
+	Key   interface{}
+	Value interface{} `hash:"ignore"` // 表明在计算哈希值的时候，这个域不参与计算
+	next  *dictEntry  `hash:"ignore"` // 表明在计算哈希值的时候，这个域不参与计算
 }
 
-func NewDictEntry(has int, key, value interface{}, next *dictEntry) *dictEntry {
+func NewDictEntry(key, value interface{}, next *dictEntry) *dictEntry {
 	return &dictEntry{
-		key: key, value: value, next: next,
+		Key: key, Value: value, next: next,
 	}
 }
 
 func (de *dictEntry) getKey() interface{} {
-	return de.key
+	return de.Key
 }
 
 func (de *dictEntry) getValue() interface{} {
-	return de.value
+	return de.Value
 }
 
 func (de *dictEntry) hasCode() uint64 {
 	hash, _ := hashstructure.Hash(de, nil)
 	return hash
 }
+
+func (de *dictEntry) equals(obj interface{}) bool {
+	switch obj.(type) {
+	case *dictEntry, dictEntry:
+		if instance, ok := obj.(*dictEntry); ok && instance != nil {
+			return de.Key == instance.Key && de.Value == instance.Value
+		}
+		if instance, ok := obj.(dictEntry); ok {
+			return de.Key == instance.Key && de.Value == instance.Value
+		}
+	default:
+		return false
+	}
+	return false
+}
+
+/************************************   segment   ***************************************/
+
+type segment struct {
+	
+}
+
+/************************************     dict    ***************************************/
+
+type Dict struct {
+}
+
--- raw_type/dict_test.go
+++ raw_type/dict_test.go
@@ -1,1 +1,51 @@
 package raw_type
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+var _ = Describe("test dictEntry equal", func() {
+	It("should equal", func() {
+		entry1 := NewDictEntry("Key", "Value", nil)
+		entry2 := NewDictEntry("Key", "Value", nil)
+		Expect(entry1.equals(entry2)).To(Equal(true))
+		Expect(entry1.hasCode()).To(Equal(entry2.hasCode()))
+
+		entry3 := NewDictEntry("Key", "Value", entry1)
+		Expect(entry1.equals(entry3)).To(Equal(true))
+		Expect(entry1.hasCode()).To(Equal(entry3.hasCode()))
+
+		var entry4 dictEntry
+		entry4.Key = "Key"
+		entry4.Value = "Value"
+		Expect(entry1.equals(entry4)).To(Equal(true))
+		Expect(entry1.hasCode()).To(Equal(entry4.hasCode()))
+	})
+
+	It("should not equal", func() {
+		entry1 := NewDictEntry("key1", "Value", nil)
+		entry2 := NewDictEntry("key2", "Value", nil)
+		Expect(entry1.equals(entry2)).To(Equal(false))
+		Expect(entry1.hasCode()).NotTo(Equal(entry2.hasCode()))
+
+		entry3 := NewDictEntry("Key", "Value", nil)
+		var entry4 dictEntry
+		Expect(entry3.equals(entry4)).To(Equal(false))
+	})
+
+	It("compare diff type", func() {
+		entry1 := NewDictEntry("Key", "Value", nil)
+		entry2 := map[string]string{
+			"Key": "Key", "Value": "Value",
+		}
+		Expect(entry1.equals(entry2)).To(Equal(false))
+	})
+
+})
+
+func TestDict(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Test Dict")
+}
