eccfdec89357244b9ff05a7f1ae892abf36c7395
MingjiLi <li_mingji@126.com>
2018-04-12 00:52:26 +0800

    #32 redis go dict

--- raw_type/dict.go
+++ raw_type/dict.go
@@ -7,21 +7,13 @@ import (
 )
 
 const (
-	/**
-	The largest possible table capacity.
-	*/
-	MaximumCapacity = 1 << 30
-
-	/**
-	  The default initial table capacity.  Must be a power of 2
-	  (i.e., at least 1) and at most MaximumCapacity.
-	*/
-	DefaultCapacity = 1 << 4
-
-	LoadFactory = 0.75
-
-	// 不使用锁情况下最多尝试次数
-	MaxScanRetries = 2
+	MaximumCapacity         = 1 << 30
+	DefaultCapacity         = 1 << 4
+	DefaultConcurrencyLevel = 16
+	MaxSegments             = 1 << 16
+	MinSegmentTableCapacity = 2
+	LoadFactory             = 0.75
+	MaxScanRetries          = 2 // 不使用锁情况下最多尝试次数
 )
 
 /************************************  dictEntry  ***************************************/
@@ -96,17 +88,17 @@ func newSegment(capacity int, lf float64, threshold int) *segment {
 
 	现在的做法是直接上锁
 */
-func (seg *segment) put(key, value interface{}) interface{} {
+func (seg *segment) put(hashCode int, key, value interface{}) interface{} {
 	seg.locker.Lock()
 	defer seg.locker.Unlock()
 
 	var oldValue interface{}
-	index := hash(key) % len(seg.table)
+	index := hashCode % len(seg.table)
 	e := seg.table[index]
 	for true {
 		if e != nil {
 			// 如果找到相同元素，先记录oldValue，再覆盖
-			if hash(key) == e.hash && reflect.DeepEqual(key, e.Key) {
+			if hashCode == e.hash && reflect.DeepEqual(key, e.Key) {
 				oldValue = e.Value
 				e.Value = value
 				seg.modCount += 1
@@ -115,7 +107,7 @@ func (seg *segment) put(key, value interface{}) interface{} {
 			e = e.next
 		} else {
 			// 如果遍历之后没有找到key相同的元素，则利用头插法插入新node
-			node := NewDictEntry(hash(key), key, value, seg.table[index])
+			node := NewDictEntry(hashCode, key, value, seg.table[index])
 			// 如果节点容量达到了rehash的条件，那么就进行rehash
 			if seg.count+1 > seg.threshold && len(seg.table) < MaximumCapacity {
 				seg.rehash(node)
@@ -143,7 +135,7 @@ func (seg *segment) rehash(node *dictEntry) {
 	oldTable := seg.table
 	oldCapacity := len(seg.table)
 	newCapacity := oldCapacity << 1
-	threshold := newCapacity * LoadFactory
+	threshold := int(float32(newCapacity) * LoadFactory)
 
 	newTable := newSegment(newCapacity, LoadFactory, threshold)
 	// 将老table中的数据迁移到新table中去
@@ -173,19 +165,24 @@ func (seg *segment) rehash(node *dictEntry) {
 	seg.table = newTable.table
 }
 
-// 删除segment中的特定元素
-func (seg *segment) remove(key, value interface{}) interface{} {
+/*
+删除segment中的特定元素,
+	如果value == nil 则删除key相等的。
+	如果value != nil 则删除key相等且value相等的。
+*/
+func (seg *segment) remove(hashCode int, key, value interface{}) interface{} {
 	seg.locker.Lock()
 	defer seg.locker.Unlock()
 	var oldValue interface{}
 
-	idx := hash(key) % len(seg.table)
+	idx := hashCode % len(seg.table)
 	if seg.table[idx] == nil {
 		return nil
 	}
 	var pre *dictEntry
 	for cur := seg.table[idx]; cur != nil; cur = cur.next {
-		if cur.hash == hash(key) && reflect.DeepEqual(key, cur.Key) && reflect.DeepEqual(value, cur.Value) {
+		if cur.hash == hashCode && reflect.DeepEqual(key, cur.Key) &&
+			(value == nil || reflect.DeepEqual(value, cur.Value)) {
 			if pre == nil {
 				seg.table[idx] = cur.next
 			} else {
@@ -203,18 +200,19 @@ func (seg *segment) remove(key, value interface{}) interface{} {
 
 /**
 用newValue替换segment中key对应的oldValue
+	@param hashCode: key对应的哈希值，节省计算
 	@param key:
 	@param oldValue: key对应的原有的值
 	@param newValue: 需要替换成为的值
 */
-func (seg *segment) replace(key, oldValue, newValue interface{}) bool {
+func (seg *segment) replace(hashCode int, key, oldValue, newValue interface{}) bool {
 	seg.locker.Lock()
 	defer seg.locker.Unlock()
 
-	index := hash(key) % len(seg.table)
+	index := hashCode % len(seg.table)
 	for e := seg.table[index]; e != nil; e = e.next {
 		// 查找目标元素
-		if hash(key) == e.hash && reflect.DeepEqual(key, e.Key) && reflect.DeepEqual(oldValue, e.Value) {
+		if hashCode == e.hash && reflect.DeepEqual(key, e.Key) && reflect.DeepEqual(oldValue, e.Value) {
 			e.Value = newValue
 			seg.modCount += 1
 			return true
@@ -243,11 +241,244 @@ type Dict struct {
 }
 
 func NewDict() *Dict {
+	return NewDictWithCapacity(DefaultCapacity)
+}
+
+func NewDictWithCapacity(capacity int) *Dict {
+	return NewDictWithCapacityAndConcurrencyLevel(capacity, DefaultConcurrencyLevel)
+}
+
+func NewDictWithCapacityAndConcurrencyLevel(capacity, concurrencyLevel int) *Dict {
+	if capacity < 0 || concurrencyLevel <= 0 {
+		panic("Illegal argument exception")
+	}
+	if concurrencyLevel > MaxSegments {
+		concurrencyLevel = MaxSegments
+	}
+	if capacity > MaximumCapacity {
+		capacity = MaximumCapacity
+	}
+	actualConcurrentLevel := 1
+	for actualConcurrentLevel < concurrencyLevel {
+		actualConcurrentLevel = actualConcurrentLevel << 1
+	}
+	expectedSegmentCap := capacity / actualConcurrentLevel
+	if expectedSegmentCap*actualConcurrentLevel < capacity {
+		expectedSegmentCap += 1
+	}
+	actualSegmentCap := MinSegmentTableCapacity
+	for actualSegmentCap < expectedSegmentCap {
+		actualSegmentCap <<= 1
+	}
+
+	segments := make([]*segment, concurrencyLevel)
+	for i := 0; i < concurrencyLevel; i++ {
+		segments[i] = newSegment(expectedSegmentCap, LoadFactory, int(float32(expectedSegmentCap)*LoadFactory))
+	}
+	return &Dict{
+		segments: segments,
+	}
+}
+
+func (dict *Dict) IsEmpty() bool {
+	sum := 0
+	for i := 0; i < len(dict.segments); i++ {
+		if dict.segments[i] != nil {
+			if dict.segments[i].count != 0 {
+				return false
+			}
+			sum += dict.segments[i].modCount
+		}
+	}
+	if sum != 0 {
+		for i := 0; i < len(dict.segments); i++ {
+			if dict.segments[i] != nil {
+				if dict.segments[i].count != 0 {
+					return false
+				}
+				sum -= dict.segments[i].modCount
+			}
+		}
+		if sum != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+/*
+	TODO lmj 改造成先尝试几次然后再加锁的逻辑
+*/
+func (dict *Dict) Size() int {
+	for i := 0; i < len(dict.segments); i++ {
+		dict.segments[i].locker.Lock()
+	}
+	defer func() {
+		for i := 0; i < len(dict.segments); i++ {
+			dict.segments[i].locker.Unlock()
+		}
+	}()
+
+	var isOverflow bool
+	var size int
+	for i := 0; i < len(dict.segments); i++ {
+		if dict.segments[i] == nil {
+			continue
+		}
+		curSize := dict.segments[i].count
+		if curSize < 0 || size+curSize < 0 {
+			isOverflow = true
+			break
+		}
+		size += curSize
+	}
+	if isOverflow {
+		return MaximumCapacity
+	}
+	return size
+}
+
+/*
+	根据hashCode先找到segment，再找dictEntry
+*/
+func (dict *Dict) Get(key interface{}) interface{} {
+	hashCode := hash(key)
+	segmentIdx := hashCode % len(dict.segments)
+	if dict.segments[segmentIdx] != nil {
+		idx := hashCode % len(dict.segments[segmentIdx].table)
+		if dict.segments[segmentIdx].table[idx] != nil {
+			for e := dict.segments[segmentIdx].table[idx]; e != nil; e = e.next {
+				if e.hash == hashCode && reflect.DeepEqual(e.Key, key) {
+					return e.Value
+				}
+			}
+		}
+	}
+	return nil
+}
+
+/*
+	和Get思路相同
+*/
+func (dict *Dict) ContainsKey(key interface{}) bool {
+	return dict.Get(key) != nil
+}
+
+/*
+	TODO 修改先尝试，再加锁
+*/
+func (dict *Dict) ContainsValue(value interface{}) bool {
+	for i := 0; i < len(dict.segments); i++ {
+		dict.segments[i].locker.Lock()
+	}
+	defer func() {
+		for i := 0; i < len(dict.segments); i++ {
+			dict.segments[i].locker.Unlock()
+		}
+	}()
+
+	for i := 0; i < len(dict.segments); i++ {
+		for j := 0; dict.segments[i] != nil && j < len(dict.segments[i].table); j++ {
+			for e := dict.segments[i].table[j]; e != nil; e = e.next {
+				if reflect.DeepEqual(value, e.Value) {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func (dict *Dict) Contains(value interface{}) bool {
+	return dict.ContainsValue(value)
+}
+
+func (dict *Dict) Put(key, value interface{}) {
+	if key == nil || value == nil {
+		panic("PUT key or value null pointer exception")
+	}
+	hashCode := hash(key)
+	segmentIdx := hashCode % len(dict.segments)
+
+	dict.segments[segmentIdx].locker.Lock()
+	defer dict.segments[segmentIdx].locker.Unlock()
+
+	dict.segments[segmentIdx].put(hashCode, key, value)
+}
+
+/*
+	TODO lmj
+*/
+func (dict *Dict) PutAll(otherDict *Dict) {
+
+}
+
+func (dict *Dict) Remove(key, value interface{}) interface{} {
+	if key == nil {
+		panic("REMOVE key null pointer exception")
+	}
+	hashCode := hash(key)
+	segmentIdx := hashCode % len(dict.segments)
+
+	dict.segments[segmentIdx].locker.Lock()
+	defer dict.segments[segmentIdx].locker.Unlock()
+
+	return dict.segments[segmentIdx].remove(hashCode, key, value)
+}
+
+func (dict *Dict) RemoveKey(key interface{}) interface{} {
+	return dict.Remove(key, nil)
+}
+
+func (dict *Dict) Replace(key, oldValue, newValue interface{}) interface{} {
+	if key == nil {
+		panic("REPLACE key null pointer exception")
+	}
+	hashCode := hash(key)
+	segmentIdx := hashCode % len(dict.segments)
+
+	dict.segments[segmentIdx].locker.Lock()
+	defer dict.segments[segmentIdx].locker.Unlock()
+
+	return dict.segments[segmentIdx].replace(hashCode, key, oldValue, newValue)
+}
+
+/*
+	TODO 这样先都锁起来，再进行操作不会导致死锁吗？为啥不是用一个锁一个呢？
+	TODO 明显的资源竞争和依赖
+*/
+func (dict *Dict) clear() {
+	for i := 0; i < len(dict.segments); i++ {
+		dict.segments[i].locker.Lock()
+	}
+	defer func() {
+		for i := 0; i < len(dict.segments); i++ {
+			dict.segments[i].locker.Unlock()
+		}
+	}()
+	for i := 0; i < len(dict.segments); i++ {
+		if dict.segments[i] != nil {
+			dict.segments[i].clear()
+		}
+	}
+}
+
+/*
+	TODO lmj
+*/
+func (dict *Dict) KeySet() map[interface{}]bool {
+	return nil
+}
+
+/*
+	TODO lmj
+*/
+func (dict *Dict) Values() map[interface{}]bool {
 	return nil
 }
 
 /************************************     common   **************************************/
 func hash(value interface{}) int {
-	hash, _ := hashstructure.Hash(value, nil)
-	return int(hash)
+	hashCode, _ := hashstructure.Hash(value, nil)
+	return int(hashCode)
 }
