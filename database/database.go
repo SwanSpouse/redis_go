58f1dad52c290e65bedaa717fc08ba0a6351e66c
李洺吉 <mingjili@creditease.cn>
2018-05-04 13:00:26 +0800

    #95 change map[string]TBase to dict

--- database/database.go
+++ database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"redis_go/loggers"
+	"redis_go/raw_type"
 	"sync/atomic"
 )
 
@@ -9,23 +11,29 @@ var (
 )
 
 type Database struct {
-	id   uint64           // 数据库编号
-	dict map[string]TBase // 数据库
+	id   uint64         // 数据库编号
+	dict *raw_type.Dict // 数据库
 }
 
 func NewDatabase() *Database {
 	return &Database{
 		id:   atomic.AddUint64(&databaseInc, 1),
-		dict: make(map[string]TBase),
+		dict: raw_type.NewDict(),
 	}
 }
 
 // 获取Key在数据库中对应的Value
 func (db *Database) SearchKeyInDB(key string) TBase {
-	if obj, ok := db.dict[key]; ok && !obj.IsExpired() {
-		return obj
+	if obj := db.dict.Get(key); obj == nil {
+		return nil
+	} else {
+		if tBase, ok := obj.(TBase); !ok || tBase.IsExpired() {
+			loggers.Errorf("illegal value in database.dict or tBase is expired. key %s", key)
+			return nil
+		} else {
+			return tBase
+		}
 	}
-	return nil
 }
 
 func (db *Database) SearchKeysInDB(keys []string) ([]TBase, error) {
@@ -41,15 +49,14 @@ func (db *Database) SearchKeysInDB(keys []string) ([]TBase, error) {
 
 // 将TBase写入到redis database
 func (db *Database) SetKeyInDB(key string, obj TBase) {
-	db.dict[key] = obj
+	db.dict.Put(key, obj)
 }
 
 // 删除redis db 中的key
 func (db *Database) RemoveKeyInDB(keys []string) int64 {
 	var successCount int64
 	for _, key := range keys {
-		if _, ok := db.dict[key]; ok {
-			delete(db.dict, key)
+		if oldValue := db.dict.RemoveKey(key); oldValue != nil {
 			successCount += 1
 		}
 	}
