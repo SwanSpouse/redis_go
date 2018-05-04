ef18d21c37a90fe86d7f5cea454d712f690841fb
李洺吉 <mingjili@creditease.cn>
2018-05-04 13:42:20 +0800

    HOTFIX fix list_test

--- raw_type/list_test.go
+++ raw_type/list_test.go
@@ -1,6 +1,7 @@
 package raw_type
 
 import (
+	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"testing"
@@ -16,12 +17,12 @@ var _ = Describe("test list ", func() {
 	BeforeEach(func() {
 		list = ListCreate()
 		for i := 9; i >= 0; i-- {
-			list = list.ListAddNodeHead(i)
+			list = list.ListAddNodeHead(fmt.Sprintf("%d", i))
 		}
 		Expect(list.length).To(Equal(10))
 
 		for i := 10; i < 20; i++ {
-			list = list.ListAddNodeTail(i)
+			list = list.ListAddNodeTail(fmt.Sprintf("%d", i))
 		}
 		Expect(list.length).To(Equal(20))
 	})
@@ -29,27 +30,27 @@ var _ = Describe("test list ", func() {
 	It("test iterator from head and tail", func() {
 		i := 0
 		for node := list.ListFirst(); node != nil; node = node.NodeNext() {
-			Expect(node.NodeValue()).To(Equal(i))
+			Expect(node.NodeValue()).To(Equal(fmt.Sprintf("%d", i)))
 			i += 1
 		}
 		i = 19
 		for node := list.ListLast(); node != nil; node = node.NodePrev() {
-			Expect(node.NodeValue()).To(Equal(i))
+			Expect(node.NodeValue()).To(Equal(fmt.Sprintf("%d", i)))
 			i -= 1
 		}
 	})
 	It("test search value in list", func() {
 		for i := 0; i < 20; i++ {
-			Expect(list.ListSearchKey(i)).NotTo(Equal(nil))
+			Expect(list.ListSearchKey(fmt.Sprintf("%d", i))).NotTo(Equal(nil))
 		}
 	})
 
 	It("test ListIndex", func() {
 		for i := 0; i < 20; i++ {
-			Expect(list.ListIndex(i).NodeValue()).To(Equal(i))
+			Expect(list.ListIndex(i).NodeValue()).To(Equal(fmt.Sprintf("%d", i)))
 		}
 		for i := 1; i < 20; i++ {
-			Expect(list.ListIndex(-i).NodeValue()).To(Equal(19 - i))
+			Expect(list.ListIndex(-i).NodeValue()).To(Equal(fmt.Sprintf("%d", 20-i)))
 		}
 	})
 })
